0f6971a61884836e122daaa76d2854f8ff7db289
Frostack <soulburn007@gmail.com>
2020-07-26 13:41:25 +0430

    🧱 add Sell Component

---
+++ src/components/Sell/Sell.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+function Sell() {
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <div>Sell</div>{' '}
+    </motion.div>
+  );
+}
+
+export default Sell;
