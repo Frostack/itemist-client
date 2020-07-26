3755c256b9f790568abf1fad57761607150ab714
Frostack <soulburn007@gmail.com>
2020-07-26 13:39:43 +0430

    🧱 add App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { verifyUser } from '../../actions/userActions';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import { AnimatePresence } from 'framer-motion';
 
-function App() {
-  return <div>Itemist</div>;
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Home from '../Home/Home';
+import Trade from '../Trade/Trade';
+import InProgress from '../InProgress/InProgress';
+import Shop from '../Shop/Shop';
+import Profile from '../Profile/Profile';
+import Sell from '../Sell/Sell';
+
+const Wrapper = styled.div`
+  background: url(./assets/background.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 100vh;
+  color: white;
+`;
+
+function App({ verifyUser }) {
+  useEffect(() => {
+    verifyUser();
+  }, [verifyUser]);
+
+  return (
+    <Router>
+      <Wrapper>
+        <Header />
+        <Route
+          render={({ location }) => (
+            <AnimatePresence exitBeforeEnter initial={false}>
+              <Switch location={location} key={location.pathname}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/trade" component={Trade} />
+                <Route exact path="/verify" component={InProgress} />
+                <Route exact path="/about" component={InProgress} />
+                <Route exact path="/shop" component={Shop} />
+                <Route exact path="/profile" component={Profile} />
+                <Route exact path="/sell" component={Sell} />
+              </Switch>
+            </AnimatePresence>
+          )}
+        />
+        <Footer />
+      </Wrapper>
+    </Router>
+  );
 }
 
-export default App;
+const mapDispatchToProps = {
+  verifyUser,
+};
+
+export default connect(null, mapDispatchToProps)(App);
